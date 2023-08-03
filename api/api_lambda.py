c09f8e6edafb33aea4300049219d4e86ed3b04b8
alirezakafaei <kafaeialireza@gmail.com>
2023-08-03 23:38:20 +1000

    First commit of the lambda function used behind API gateway

---
+++ api/api_lambda.py
@@ -0,0 +1,17 @@
+import json
+import uuid
+
+GET_RAW_PATH = "/getImage"
+CREAT_RAW_PATH = "/createImage"
+
+def lambda_handler(event, context):
+    print(event)
+    if event["rawPath"] == GET_RAW_PATH:
+        #GetPerson Path
+        print("Start Request for GetPerson")
+        #personId = event['queryStringParameters']['personId']
+        return {"firstName" : "Daniel", "lastName": "G", "email":"something@gmail.com"}
+        
+    elif event['rawPath'] == CREAT_RAW_PATH:
+        #Write to Database
+        print("Start Request for CreatePerson")
