2a212202f335220ed5e93dbcd5e9d10954d2f14d
alirezakafaei <kafaeialireza@gmail.com>
2023-08-04 22:30:37 +1000

    Accessing S3 through lambda

--- api/api_lambda.py
+++ api/api_lambda.py
@@ -21,3 +21,34 @@ def lambda_handler(event, context):
         
         
     
+#######
+
+import boto3
+
+def lambda_handler(event, context):
+    # Initialize the S3 client
+    s3 = boto3.client('s3')
+
+    # Define your S3 bucket and object key
+    bucket_name = 'your-bucket-name'
+    object_key = 'your-object-key'
+
+    try:
+        # Example: Get an object from S3
+        response = s3.get_object(Bucket=bucket_name, Key=object_key)
+        data = response['Body'].read().decode('utf-8')
+        print(data)
+
+        # Example: Put an object to S3
+        s3.put_object(Bucket=bucket_name, Key='new-object-key', Body='Hello from Lambda!')
+
+        return {
+            'statusCode': 200,
+            'body': 'S3 access from Lambda successful'
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error accessing S3: {str(e)}'
+        }
